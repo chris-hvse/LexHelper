9461f489499ed491e34fd9bd834555e7acb99868
chris.hvse <chris.hvse@gmail.com>
2017-10-09 07:52:25 -0700

    dialog.js: dialog builder function added
    Signed-off-by: chris.hvse <chris.hvse@gmail.com>

--- dialog.js
+++ dialog.js
@@ -1,4 +1,5 @@
 const LexEvent = require(__dirname + '/lex_event');
+const _  = require('lodash');
 
 class Dialog extends LexEvent {
     _defaultMatcher(event) {
@@ -25,6 +26,48 @@ class Dialog extends LexEvent {
         this.actions.unshift({
             matcher: this._defaultMatcher,
             executor: this._defaultExecutor
+        });
+    }
+
+    buildElicitSlotResponse({event, message, slot, buttons_promise}) {
+        return new Promise((resolve, reject) => {
+            buttons_promise().then((buttons) => {
+                let chunks = _.chunk(buttons, 5);
+                let genericAttachments = chunks.map((chunk) => {
+                    let buttons = chunk.map((value) => (
+                        {
+                            text: value,
+                            value: value
+                        }
+                    ));
+                    return {
+                        title: ' ',
+                        buttons: buttons
+                    };
+                })
+    
+                let response = {
+                    sessionAttributes : {},
+                    dialogAction : {
+                        type: "ElicitSlot",
+                        message: {
+                            contentType: "PlainText",
+                            content: message
+                        },
+                        intentName: event['currentIntent']['name'],
+                        slots: event['currentIntent']['slots'],
+                        slotToElicit: slot,
+                        responseCard: {
+                            version: 1,
+                            contentType: "application/vnd.amazonaws.card.generic",
+                            genericAttachments: genericAttachments
+                        }
+                    }
+                };
+                return resolve(response);
+            }).catch((err) => {
+                return reject(err);
+            });
         });
     }
 };
