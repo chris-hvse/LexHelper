d3c34872193c30dd309665f2950c0ceca885edd8
chris.hvse <chris.hvse@gmail.com>
2017-10-10 08:58:44 -0700

    eslint: tests added
    Signed-off-by: chris.hvse <chris.hvse@gmail.com>

---
+++ eslint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo ""
+echo "Linted Files"
+echo "============"
+
+# Loop through controllers.
+for filename in ./*.js; do
+    echo "$filename"
+
+    eslint $filename
+done
+
+echo ""
